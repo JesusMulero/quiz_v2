6be7005af62a64e1e1bfae7e53b95a43647a9586
JesusMulero <jesusmb90@hotmail.com>
2015-07-19 17:52:47 +0200

    Validado preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -24,18 +24,18 @@ exports.index = function(req, res) {
 	 order: [[ 'pregunta', 'ASC']]
       }
     ).then(function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
+      res.render('quizes/index', { quizes: quizes, errors: []});
     }).catch(function(error) { next(error);});
   } else { 
     models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
+      res.render('quizes/index', { quizes: quizes, errors: []});
     }).catch(function(error) { next(error);});
   }
 };
  
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -44,7 +44,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -52,17 +52,22 @@ exports.new = function(req, res){
   var quiz = models.Quiz.build(
     { pregunta: "Pregunta", respuesta: "Respuesta"}
   );
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res){
   var quiz = models.Quiz.build( req.body.quiz );
 
-  // guardar en BD pregunta y respuesta
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');  
-  }) // redireccion a la lista de preguntas 
+  quiz.validate().then(function(err){
+    if (err) {
+      res.render('/quizes/new', {quiz: quiz, errors: err.errors});
+    } else {
+  	// guardar en BD pregunta y respuesta
+  	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    	  res.redirect('/quizes');  
+  	}) // redireccion a la lista de preguntas 
+    }
 };
 
 // GET /author
