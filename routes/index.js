94d94c401c3d282a8ceb28d78bf92adc94e53f0d
JesusMulero <jesusmb90@hotmail.com>
2015-08-03 00:27:00 +0200

    Borrar pregunta

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,10 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.get('/author',                      quizController.author);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 
 router.post('/quizes/create',              quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 module.exports = router;
